d2a6a6e28dbd3b2f4d3e01beb7aa3cd0daaa5ada
John Labovitz <johnl@johnlabovitz.com>
2011-05-06 12:29:53 -0700

    Ensure division doesn't truncate.

--- lib/photo_utils/extensions/numeric.rb
+++ lib/photo_utils/extensions/numeric.rb
@@ -7,11 +7,11 @@ INCHES_PER_METER = 39.3700787402
 class Numeric
   
   def feet
-    PhotoUtils::Length.new(self * 12 / INCHES_PER_METER * 1000)
+    PhotoUtils::Length.new(self.to_f * 12 / INCHES_PER_METER * 1000)
   end
   
   def inches
-    PhotoUtils::Length.new(self / INCHES_PER_METER * 1000)
+    PhotoUtils::Length.new(self.to_f / INCHES_PER_METER * 1000)
   end
   
   def meters
