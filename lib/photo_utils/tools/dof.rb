1c22d7961de98cf93560994be40085929e3592a3
John Labovitz <johnl@johnlabovitz.com>
2011-05-23 21:02:57 -0700

    Update DOF tool.

--- lib/photo_utils/tools/dof.rb
+++ lib/photo_utils/tools/dof.rb
@@ -7,60 +7,35 @@ module PhotoUtils
     class DOF < Tool
       
       def run(args)
+        
+        cameras = []
+        cameras << Camera[/R-D1/]
+        cameras[-1].lens = cameras[-1].lenses.find { |l| l.focal_length == 50.mm }
 
-        Camera.cameras.each do |camera|
+        cameras << Camera[/Ikon/]
+        cameras[-1].lens = cameras[-1].lenses.find { |l| l.focal_length == 85.mm }
+
+        cameras << Camera[/RF645/]
+        cameras[-1].lens = cameras[-1].lenses.find { |l| l.focal_length == 100.mm }
+        
+        cameras << Camera[/C330/]
+        cameras[-1].lens = cameras[-1].lenses.find { |l| l.focal_length == 180.mm }
+        
+        cameras.each do |camera|
   
           scene = Scene.new
           scene.camera = camera
-              
-          # calculate average brightness, by analyzing test exposures on site
+          scene.camera.shutter = 1.0 / 125
+          scene.subject_distance = 25.feet
+          # scene.camera.lens.aperture = scene.aperture_for_depth_of_field(scene.subject_distance - 3.feet, scene.subject_distance + 3.feet)
+          scene.camera.lens.aperture = 4.5
           scene.sensitivity = 1600
-          exposures = [
-            # aperture, shutter sheed
-            [4, 30],
-          ]
-          brightnesses = []
-          exposures.each do |aperture, time|
-            scene.camera.lens.aperture = aperture
-            scene.camera.shutter = 1.0 / time
-            brightnesses << scene.brightness
-          end
-          scene.brightness = brightnesses.mean
-  
-          scene.sensitivity = 1600
-          scene.camera.lens.aperture = nil
-          scene.camera.shutter = nil
-  
-          # apertures = [4, 5.6, 8, 11, 16]
-          apertures = [2.8]
-  
-          closest_focus = 20 # feet
-          farthest_focus = 50 # feet
-  
-          closest_focus.upto(farthest_focus).each do |feet|
-            scene.subject_distance = feet.feet
-    
-            puts "\t\t" + "subject_distance: #{scene.subject_distance.to_s(:imperial)}"
-    
-            apertures.each do |aperture|
-              scene.camera.lens.aperture = aperture
-              
-              # capture most of a face
-              next if scene.total_depth_of_field < 8.inches
-        
-              # rule of thumb: shutter speed (in fractions of a second) should be at least the focal length (in mm)
-              next if 1.0/scene.camera.shutter < scene.camera.lens.focal_length
-        
-              # but I can usually hand-hold to 1/30
-              next if scene.camera.shutter < 1.0/30
-              
-              puts "\t\t\t" + [
-                "#{scene.camera.lens.aperture} @ #{scene.camera.shutter}",
-                scene.exposure,
-                "#{scene.total_depth_of_field.to_s(:imperial)} (-#{scene.near_distance_from_subject.to_s(:imperial)} .. +#{scene.far_distance_from_subject.to_s(:imperial)})"
-              ].join("\t")
-            end
-          end
+          scene.set_exposure
+          
+          puts "#{camera.name}"
+          puts "\t" + "FOV: #{scene.field_of_view(scene.subject_distance).to_s(:imperial)}"
+          puts "\t" + "DOF: #{scene.total_depth_of_field.to_s(:imperial)} (-#{scene.near_distance_from_subject.to_s(:imperial)}/+#{scene.far_distance_from_subject.to_s(:imperial)})"
+          puts
         end
       end
       
