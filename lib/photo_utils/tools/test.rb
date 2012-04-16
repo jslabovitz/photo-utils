e146aa14509f50a04f58ab65a8ebb877f678113e
John Labovitz <johnl@johnlabovitz.com>
2012-04-16 10:00:53 -0700

    Tweaking.

--- lib/photo_utils/tools/test.rb
+++ lib/photo_utils/tools/test.rb
@@ -9,44 +9,67 @@ module PhotoUtils
       def run(args)
         
         camera = Camera[/Eastman/]
+        fov = Frame.new(6.feet, 3.feet)
+        # dof = 4.feet
+        sensitivity = 100
+        brightness = nil
+        aperture = 4.5
+        shutter = 1.0/25
+
+        header = "%-20.20s | %-50.50s | %-12.12s | %9s | %7s | %10s | %14s | %5s | %s"
         
-        fov = Frame.new(4.feet, 3.feet)
+        puts header % %w{
+          format
+          lens
+          FOV
+          subj-dist
+          DOF
+          brightness
+          exposure
+          comp
+          APEX
+        }
         
         camera.formats.each do |format|
+          next unless format.name =~ /5x7/
           camera.format = format
           camera.lenses.sort_by(&:focal_length).each do |lens|
+            next unless lens.name =~ /12"/
             camera.lens = lens
-              scene = Scene.new
-              scene.sensitivity = 3000
-              scene.brightness = 100
-              scene.camera = camera
-              scene.subject_distance = scene.subject_distance_for_field_of_view(fov)
-              camera.lens.aperture = scene.aperture_for_depth_of_field(scene.subject_distance - 1.feet, scene.subject_distance + 1.feet)
-              camera.shutter = nil
-              scene.set_exposure
-              
-              if scene.working_aperture > 0 && scene.working_aperture < camera.lens.min_aperture
-                exp_comp = scene.working_aperture.to_v - camera.lens.aperture.to_v
-                exp_comp = (exp_comp * 2).to_i / 2
-                exp_comp = if exp_comp < 0
-                  "-#{exp_comp.abs}"
-                elsif exp_comp > 0
-                  "+#{exp_comp}"
-                else
-                  nil
-                end
+            scene = Scene.new
+            scene.sensitivity = sensitivity
+            # scene.brightness = brightness
+            scene.camera = camera
+            scene.subject_distance = scene.subject_distance_for_field_of_view(fov)
+            # camera.lens.aperture = scene.aperture_for_depth_of_field(scene.subject_distance - (dof / 2), scene.subject_distance + (dof / 2))
+            camera.shutter = shutter
+            scene.set_exposure
+            # ;;scene.print
+            
+            if scene.working_aperture > 0 && scene.working_aperture < camera.lens.min_aperture
+              exp_comp = scene.working_aperture.to_v - camera.lens.aperture.to_v
+              exp_comp = (exp_comp * 2).to_i / 2
+              exp_comp = if exp_comp < 0
+                "-#{exp_comp.abs}"
+              elsif exp_comp > 0
+                "+#{exp_comp}"
+              else
+                nil
               end
-                  
-              puts "%-30.30s | %-50.50s | %-15.15s | %10s | %10s @ %-5.5s %s" % [
-                format,
-                lens,
-                scene.field_of_view(scene.subject_distance).to_s(:imperial),
-                scene.subject_distance.to_s(:imperial),
-                camera.lens.aperture,
-                camera.shutter,
-                exp_comp ? "[#{exp_comp} EV]" : ''
-              ]
             end
+                
+            puts header % [
+              format,
+              lens,
+              scene.field_of_view(scene.subject_distance).to_s(:imperial),
+              scene.subject_distance.to_s(:imperial),
+              scene.total_depth_of_field.to_s(:imperial),
+              scene.brightness.to_s(:lux_eq),
+              '%s @ %s' % [scene.camera.lens.aperture, camera.shutter],
+              exp_comp ? "[#{exp_comp} EV]" : '',
+              scene.exposure.to_s,
+            ]
+          end
         end
       end
       
