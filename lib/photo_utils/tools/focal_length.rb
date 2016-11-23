5baa59f9317da830849d3c3ca41c0de8103ebcb9
John Labovitz <johnl@johnlabovitz.com>
2016-11-23 10:53:41 -0500

    Use ARGV for arguments.

--- lib/photo_utils/tools/focal_length.rb
+++ lib/photo_utils/tools/focal_length.rb
@@ -7,10 +7,10 @@ module PhotoUtils
     class FocalLength < Tool
 
       def run
-        from_focal_length, from_format = @args.shift.split(':')
+        from_focal_length, from_format = ARGV.shift.split(':')
         from_focal_length = from_focal_length.to_i
         from_format = (Format[from_format || '35'] or raise "Unknown format: #{from_format.inspect}")
-        to_formats = @args.first ? @args.shift.split(',') : %w{1/1.7 APS-C APS-H 35 6x4.5 6x6 6x7 5x7}
+        to_formats = ARGV.first ? ARGV.shift.split(',') : %w{1/1.7 APS-C APS-H 35 6x4.5 6x6 6x7 5x7}
         to_formats.map { |f| Format[f] or raise "Unknown format: #{f.inspect}" }.each do |to_format|
           scene = Scene.new
           scene.format = from_format
