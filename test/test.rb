08f17d8dd10cc5e53a19adb730a6f574a3f27e5b
John Labovitz <johnl@johnlabovitz.com>
2018-06-07 11:33:39 -0400

    Add test handler.

---
+++ test/test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/power_assert'
+
+require 'photo-utils'
+
+module PhotoUtils
+
+  class Test < MiniTest::Test
+
+    def setup
+    end
+
+    def teardown
+    end
+
+  end
+
+end
