5cfdb9f4369ebeb1003af6f0ef602f712111ed4e
John Labovitz <johnl@johnlabovitz.com>
2018-06-10 18:27:21 -0400

    Fix test to work correctly with cd/m2 units. Tweak whitespace.

--- test/brightness_test.rb
+++ test/brightness_test.rb
@@ -5,22 +5,23 @@ module PhotoUtils
   class TestBrightness < Test
 
     def setup
-  	  @b = BrightnessValue.new(1)
+      @cdm2 = 3.4
+      @b = BrightnessValue.new(@cdm2)
     end
 
-    def value
-  	  assert { @b.to_f == 1 }
+    def test_value
+      assert { @b.to_f == @cdm2 }
       assert { @b.to_v.round == 0 }
     end
 
     def test_format_as_string
-  	  assert { @b.to_s == '1 cd/m2' }
-  	  assert { @b.to_s(:value) == 'Bv:0' }
+      assert { @b.to_s == "#{@cdm2} cd/m2" }
+      assert { @b.to_s(:value) == 'Bv:0' }
     end
 
     def test_increments_and_decrements
-  	  assert { @b.incr.round == BrightnessValue.new(@b * 2).round }
-  	  assert { @b.decr.round == BrightnessValue.new(@b / 2).round }
+      assert { @b.incr.round == BrightnessValue.new(@b * 2).round }
+      assert { @b.decr.round == BrightnessValue.new(@b / 2).round }
     end
 
   end
