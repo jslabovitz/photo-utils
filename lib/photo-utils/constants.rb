c0df9a578d1e54f2bf9f8319257d4c6294275180
John Labovitz <johnl@johnlabovitz.com>
2018-06-10 18:43:39 -0400

    Add comments on constants, and additional options.

--- lib/photo-utils/constants.rb
+++ lib/photo-utils/constants.rb
@@ -2,14 +2,27 @@ module PhotoUtils
 
   module Constants
 
+    # https://en.wikipedia.org/wiki/Light_meter#Calibration_constants
+
     N = 2 ** Rational(-7, 4)    # APEX
     # N = Rational(1, 3.125)      # EXIF
 
+    #
+    # incident-light meter calibration constant
+    #
+
     C = 224     # APEX
+    # C = 250   # flat receptor
+    # C = 320   # Minolta
     # C = 340   # Sekonic
 
+    #
+    # reflected-light meter calibration constant
+    #
+
     K = 11.4    # APEX
-    # K = 12.5  # Sekonic
+    # K = 12.5  # Canon, Nikon, Sekonic
+    # K = 14    # Minolta, Kenko, Pentax
 
     ISO_BASE = 3.125
 
