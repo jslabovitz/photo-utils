ef4b496a7cb355e7905faf34d6a65242e20efa7b
John Labovitz <johnl@johnlabovitz.com>
2018-06-11 11:23:35 -0400

    Add camera test.

---
+++ test/test_camera.rb
@@ -0,0 +1,24 @@
+require_relative 'test'
+
+module PhotoUtils
+
+  class TestCamera < Test
+
+    def setup
+      @camera = Camera[/Generic 35mm/]
+    end
+
+    def test
+      assert { @camera.name == 'Generic 35mm' }
+      assert { @camera.format.name == '35' }
+      assert { @camera.lens.name == '50mm' }
+      assert { @camera.angle_of_view == 47 }
+      lens = @camera.lens
+      assert { lens == @camera.normal_lens }
+      assert { lens.min_aperture == 22 }
+      assert { lens.max_aperture == 2.8 }
+    end
+
+  end
+
+end
